35ee336adc3993d2f4e30d45308d64c74d6e45c3
Mayur Mamoria <mayur.mamoria@gmail.com>
2019-03-05 07:41:52 +0530

    Pagination in employee forms, customer list and admin home

--- src/app/customer-services/customer-services.component.ts
+++ src/app/customer-services/customer-services.component.ts
@@ -31,6 +31,13 @@ export class CustomerServicesComponent implements OnInit {
     liveChatServices: any;
     callAnsweringServices: any;
     addressLicenseServices: any;
+    totalPages: any;
+    totalRecords: any;
+    skipEntries: number = 0;
+    showPrevious: boolean = false;
+    showNext: boolean = false;
+    showFirst: boolean = false;
+    showLast: boolean = false;
 
     constructor(private authService: AuthService, private http: Http, private activatedRoute: ActivatedRoute, private router: Router, public auth: FormioAuthService) { }
 
@@ -40,7 +47,7 @@ export class CustomerServicesComponent implements OnInit {
             this.activatedRoute.queryParams.subscribe(params => {
                 this.accountId = params['accountId'];
                 this.getAccountDetails();
-                this.getUsersForAccount();
+                this.getUsersForAccount(0, '', true);
             });
         });
     }
@@ -58,13 +65,17 @@ export class CustomerServicesComponent implements OnInit {
         });
     }
 
-    getUsersForAccount() {
+    getUsersForAccount(skipEntries, searchUserName, firstTime) {
+        if (firstTime) {
+            this.skipEntries = 0;
+        }
         this.loading = true;
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
         headers.append("x-jwt-token", this.authService.getJwtToken());
         let options = new RequestOptions({ headers: headers });
-        this.http.get(this.appConfig.appUrl + '/customeraccount/submission?data.account._id=' + this.accountId, options).subscribe((res: any) => {
+        this.http.get(this.appConfig.appUrl + '/customeraccount/submission?data.account._id=' + this.accountId + '&skip=' + skipEntries, options).subscribe((res: any) => {
+            this.pagination((res.headers._headers.get('content-range')[0]).split("/").pop());
             let respon = res.json();
             this.customerUser = respon;
             this.loading = false;
@@ -130,6 +141,59 @@ export class CustomerServicesComponent implements OnInit {
             this.addressLicenseServices = res.json();
             this.loading = false;
         });
+    }
+
+    showDataByPage(type, skipEntries: number, userType) {
+        skipEntries = Number(skipEntries);
+        if (this.skipEntries == 0 && type == 'next') {
+            this.skipEntries = 10;
+        } else if (this.skipEntries > 0 && type == 'next') {
+            this.skipEntries = this.skipEntries + skipEntries;
+        } else if (this.skipEntries > 0 && type == 'previous') {
+            this.skipEntries = this.skipEntries - skipEntries;
+        } else if (type == 'first') {
+            this.skipEntries = 0;
+        } else if (type == 'last') {
+            //Emit last digit of total records and replaced with 0 to skip starting pages
+            this.skipEntries = Number(this.totalRecords.slice(0, -1) + '0');
+        } else {
+            this.skipEntries = skipEntries;
+        }
+
+        if (userType == "customers") {
+            this.getUsersForAccount(this.skipEntries, '', false);
+        }
+    }
+
+    pagination(recordsFromServer) {
+        this.showPrevious = false;
+        this.showNext = false;
+        this.totalRecords = recordsFromServer;
+        var totalRecords = recordsFromServer;
+        this.totalPages = "";
+        //Get total pages
+        this.totalPages = Math.ceil(totalRecords / 10);
+        if (totalRecords <= 10) {
+            this.showNext = false;
+            this.showPrevious = false;
+            this.showLast = false;
+            this.showFirst = false;
+        } else if (this.skipEntries == 0) {
+            this.showNext = true;
+            this.showPrevious = false;
+            this.showLast = true;
+            this.showFirst = false;
+        } else if ((totalRecords - this.skipEntries) >= 10) {
+            this.showNext = true;
+            this.showPrevious = true;
+            this.showFirst = true;
+            this.showLast = true;
+        } else if ((totalRecords - this.skipEntries) <= 10) {
+            this.showNext = false;
+            this.showPrevious = true;
+            this.showFirst = true;
+            this.showLast = false;
+        }
     }
 
 }
--- src/app/employee-home/employee-home.component.ts
+++ src/app/employee-home/employee-home.component.ts
@@ -16,6 +16,13 @@ export class EmployeeHomeComponent implements OnInit {
     loading: boolean = false;
     availableForms: any;
     firstAndLastName: any;
+    totalPages: any;
+    totalRecords: any;
+    skipEntries: number = 0;
+    showPrevious: boolean = false;
+    showNext: boolean = false;
+    showFirst: boolean = false;
+    showLast: boolean = false;
 
     constructor(private authService: AuthService, private http: Http, private auth: FormioAuthService) { }
 
@@ -23,20 +30,78 @@ export class EmployeeHomeComponent implements OnInit {
         this.auth.ready.then(() => {
             this.showEmployeeScreen = this.authService.showEmployeeScreen;
             this.firstAndLastName = this.authService.getFirstAndLastName();
-            this.getEmployeeDetails();
+            this.getEmployeeForms(0, '', true);
         });
     }
 
-    getEmployeeDetails(): void {
+    getEmployeeForms(skipEntries, searchUserName, firstTime): void {
+        if (firstTime) {
+            this.skipEntries = 0;
+        }
         this.loading = true;
         var headers = new Headers();
         headers.append("x-jwt-token", this.authService.getJwtToken());
         headers.append('Content-Type', 'application/json');
         let options = new RequestOptions({ headers: headers });
-        this.http.get(this.appConfig.appUrl + '/form?type=form&limit=50&submissionAccess.roles=' + this.authService.getRoleId(), options).subscribe((res: any) => {
+        this.http.get(this.appConfig.appUrl + '/form?type=form&submissionAccess.roles=' + this.authService.getRoleId()+'&skip=' + skipEntries, options).subscribe((res: any) => {
+            this.pagination((res.headers._headers.get('content-range')[0]).split("/").pop());
             let respon = res.json();
             this.availableForms = respon;
             this.loading = false;
         });
     }
+
+    showDataByPage(type, skipEntries: number, userType) {
+        skipEntries = Number(skipEntries);
+        if (this.skipEntries == 0 && type == 'next') {
+            this.skipEntries = 10;
+        } else if (this.skipEntries > 0 && type == 'next') {
+            this.skipEntries = this.skipEntries + skipEntries;
+        } else if (this.skipEntries > 0 && type == 'previous') {
+            this.skipEntries = this.skipEntries - skipEntries;
+        } else if (type == 'first') {
+            this.skipEntries = 0;
+        } else if (type == 'last') {
+            //Emit last digit of total records and replaced with 0 to skip starting pages
+            this.skipEntries = Number(this.totalRecords.slice(0, -1) + '0');
+        } else {
+            this.skipEntries = skipEntries;
+        }
+
+        if (userType == "employeeForms") {
+            this.getEmployeeForms(this.skipEntries, '', false);
+        }
+    }
+
+    pagination(recordsFromServer) {
+        this.showPrevious = false;
+        this.showNext = false;
+        this.totalRecords = recordsFromServer;
+        var totalRecords = recordsFromServer;
+        this.totalPages = "";
+        //Get total pages
+        this.totalPages = Math.ceil(totalRecords / 10);
+        if (totalRecords <= 10) {
+            this.showNext = false;
+            this.showPrevious = false;
+            this.showLast = false;
+            this.showFirst = false;
+        } else if (this.skipEntries == 0) {
+            this.showNext = true;
+            this.showPrevious = false;
+            this.showLast = true;
+            this.showFirst = false;
+        } else if ((totalRecords - this.skipEntries) >= 10) {
+            this.showNext = true;
+            this.showPrevious = true;
+            this.showFirst = true;
+            this.showLast = true;
+        } else if ((totalRecords - this.skipEntries) <= 10) {
+            this.showNext = false;
+            this.showPrevious = true;
+            this.showFirst = true;
+            this.showLast = false;
+        }
+    }
+
 }
